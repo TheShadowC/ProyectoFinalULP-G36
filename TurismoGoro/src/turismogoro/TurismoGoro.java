ea4b424f28f516d173b49af95ee49867915f2f51
Juan Carlos Quiroz <quirozjuan497@gmail.com>
2023-10-04 12:08:18 -0300

    Prueba desde el main
    prueba

--- TurismoGoro/src/turismogoro/TurismoGoro.java
+++ TurismoGoro/src/turismogoro/TurismoGoro.java
@@ -1,5 +1,9 @@
 package turismogoro;
 
+import AccesoADatos.*;
+import java.util.List;
+import turismogoro.Entidades.Ciudad;
+
 /**
  *
  * @author USUARIO
@@ -7,7 +11,27 @@ package turismogoro;
 public class TurismoGoro {
 
     public static void main(String[] args) {
-    
+        
+        Ciudad_Data c = new Ciudad_Data();
+        
+        List<Ciudad> ciudad = c.obtenerCiudades();
+        
+        for (Ciudad ciudades : ciudad) {
+            System.out.println("Ciudades: " + ciudades);
+            
+        }
+        
+        //Creamos la Ciudad y la Agregamos a la BD
+        /*Ciudad ciudad1 = new Ciudad();
+        ciudad1.setNombre("Corrientes");
+        ciudad1.setProvincia("Corrientes");
+        ciudad1.setPais("Argentina");
+        ciudad1.setEstado(true);
+        
+        c.guardarCiudad(ciudad1);*/
+        
+        //Eliminamos la Ciudad (Borrado Logico)
+        //c.eliminarCiudad(ciudad1);
     }
-    
+
 }
